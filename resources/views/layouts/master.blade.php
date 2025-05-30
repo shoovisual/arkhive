6dc53dbd60e5f4c57d2a9044e8cdf1d1fa7e7547
shoovisual <shoovisual@gmail.com>
2025-05-30 14:37:29 +0300

    pages resolved

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,7 +35,7 @@
 
 
     <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
 
     <script type="application/ld+json" class="yoast-schema-graph">

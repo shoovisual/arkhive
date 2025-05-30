b60456132374c512057cf29d2794a22932def7d2
shoovisual <shoovisual@gmail.com>
2025-05-30 17:27:05 +0300

    ui updated pt 6

--- resources/views/layouts/vendor-css.blade.php
+++ resources/views/layouts/vendor-css.blade.php
@@ -1,3 +1,6 @@
 <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">

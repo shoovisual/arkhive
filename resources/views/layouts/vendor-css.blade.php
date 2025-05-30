ab6d0c04f78841e82aa7bd202c17b67f0d24d6e4
shoovisual <shoovisual@gmail.com>
2025-05-30 14:33:31 +0300

    build added 2

--- resources/views/layouts/vendor-css.blade.php
+++ resources/views/layouts/vendor-css.blade.php
@@ -1,3 +1,3 @@
 <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />

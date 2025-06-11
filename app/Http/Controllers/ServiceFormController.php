2142d11088efc00e7adb1c8d7db25d007e3f0c1b
shoovisual <shoovisual@gmail.com>
2025-06-12 00:53:31 +0300

    UI Update v8.3

--- app/Http/Controllers/ServiceFormController.php
+++ app/Http/Controllers/ServiceFormController.php
@@ -10,78 +10,41 @@ class ServiceFormController extends Controller
 {
     public function submit(Request $request)
     {
-        $request->validate([
+        // Validate the request data
+        $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
             'phone' => 'required|string|max:20',
+            'service_name' => 'required|string|max:255',
         ]);
 
         try {
-            // Get the questions from the service
-            $questions = [];
+            // Process questions and answers
+            $questions = $request->service_questions ?? [];
             $answers = [];
             
-            if ($request->has('service_questions')) {
-                foreach ($request->service_questions as $index => $question) {
-                    $questions[] = $question;
-                    $answers[] = $request->input('question_' . $index);
-                }
+            foreach ($questions as $index => $question) {
+                $answers[] = $request->input('question_' . $index);
             }
 
-            // Validate mail configuration
-            $mailConfig = [
-                'host' => config('mail.mailers.smtp.host'),
-                'port' => config('mail.mailers.smtp.port'),
-                'username' => config('mail.mailers.smtp.username'),
-                'password' => config('mail.mailers.smtp.password'),
-                'encryption' => config('mail.mailers.smtp.encryption'),
-                'from_address' => config('mail.from.address'),
-            ];
-
-            foreach ($mailConfig as $key => $value) {
-                if (empty($value)) {
-                    \Log::error("Mail configuration error: {$key} is not set");
-                    return back()->with('error', 'Mail configuration error. Please contact the administrator.');
-                }
-            }
-
-            $toEmail = $mailConfig['from_address'];
-
-            try {
-                \Log::info('Attempting to send email with the following data:', [
-                    'to' => $toEmail,
-                    'from' => $request->email,
-                    'name' => $request->name,
-                    'service' => $request->service_name,
-                    'questions_count' => count($questions),
-                    'answers_count' => count($answers)
-                ]);
-
-                // Send email
-                $mail = new ContactFormMail(
-                    $request->name,
-                    $request->email,
-                    $request->phone,
+            // Send email
+            Mail::to('clientservices@arkhive.africa')
+                ->send(new ContactFormMail(
+                    $validatedData['name'],
+                    $validatedData['email'],
+                    $validatedData['phone'],
                     'Service Inquiry',
-                    $request->service_name,
+                    $validatedData['service_name'],
                     $request->message ?? null,
                     $questions,
                     $answers
-                );
+                ));
 
-                Mail::to($toEmail)->send($mail);
-
-                return back()->with('success', 'Thank you for your inquiry. We will get back to you soon!');
-            } catch (\Exception $mailException) {
-                \Log::error('Mail sending failed: ' . $mailException->getMessage());
-                \Log::error('Stack trace: ' . $mailException->getTraceAsString());
-                return back()->with('error', 'Sorry, there was an error sending your message. Please try again later.');
-            }
-
+            return back()->with('success', 'Thank you for your message. We will get back to you soon!');
         } catch (\Exception $e) {
-            \Log::error('Form processing error: ' . $e->getMessage());
-            \Log::error('Stack trace: ' . $e->getTraceAsString());
-            return back()->with('error', 'Sorry, there was an error processing your request. Please try again later.');
+            return back()
+                ->with('error', 'Sorry, there was an error sending your message. Please try again later.')
+                ->withInput();
         }
     }
 }

68886856af7ebe14b24366590d8c688d2a0a790a
shoovisual <shoovisual@gmail.com>
2025-06-19 09:18:14 +0300

    UI Updat v9.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,45 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
 <main id="servicesWrapper" class="flex lg:flex-row flex-col h-[100dvh] w-full overflow-hidden">
     @foreach($services as $index => $service)
-    <a href="{{ $service->url }}" class="service-slide services-card group relative overflow-hidden bg-center bg-cover transition-transform duration-500 ease-in-out grayscale-90 hover:grayscale-0 origin-left" style="background-image: url('{{ $service->cover_image }}'); transform: scaleX(1);" data-index="{{ $index }}">
+    <a href="{{ $service->url }}"
+       class="service-slide services-card group relative overflow-hidden bg-center bg-cover transition-transform duration-500 ease-in-out grayscale-90 hover:grayscale-0 origin-left"
+       style="background-image: url('{{ $service->cover_image }}');"
+       data-index="{{ $index }}">
+
         <div class="absolute inset-0 bg-black/70 overlay z-10 transition-all"></div>
+
         <div class="relative z-20 flex flex-col items-center mt-15 justify-center h-full w-full text-gray-500 text-center px-6">
-            <h4 class="hidden group-hover:block -translate-y-2 font-[600] uppercase font-[montserrat] text-white group-hover:translate-y-2 transition-all duration-500 text-4xl mb-4">{{ $service->sub_1 }}</h4>
-            <h2 class="text-4xl group-hover:text-6xl transition-all duration-500 font-[800] uppercase text-white font-[montserrat] mb-4">{{ $service->title }}</h2>
-            <h4 class="hidden group-hover:block -translate-y-2 font-[600] uppercase font-[montserrat] text-white hover:translate-y-2 transition-all duration-500 text-4xl mb-8">{{ $service->sub_2 }}</h4>
-            <div class="button hidden absolute bottom-24 group-hover:bottom-26 h-16 w-16 group-hover:h-32 group-hover:w-32 bg-black/0 border-2 border-gray-500 group-hover:border-white/0 group-hover:bg-black/50 lg:flex items-center justify-center rounded-full transition-all duration-500">
-                <i class="icon-class group-hover:hidden group-hover:rotate-45 text-4xl bi bi-plus"></i>
-                <p class="hidden text-white font-[Montserrat] text-xl group-hover:block">Go</p>
+            <!-- Sub Title 1 -->
+            <h4 class="block lg:hidden text-white text-2xl font-[600] uppercase font-[montserrat] mb-4">
+                {{ $service->sub_1 }}
+            </h4>
+            <h4 class="hidden lg:group-hover:block -translate-y-2 text-white text-4xl font-[600] uppercase font-[montserrat] lg:group-hover:translate-y-2 transition-all duration-500 mb-4">
+                {{ $service->sub_1 }}
+            </h4>
+
+            <!-- Title -->
+            <h2 class="text-4xl lg:group-hover:text-6xl transition-all duration-500 font-[800] uppercase text-white font-[montserrat] mb-4">
+                {{ $service->title }}
+            </h2>
+
+            <!-- Sub Title 2 -->
+            <h4 class="block lg:hidden text-white text-2xl font-[600] uppercase font-[montserrat] mb-4">
+                {{ $service->sub_2 }}
+            </h4>
+            <h4 class="hidden lg:group-hover:block -translate-y-2 text-white text-4xl font-[600] uppercase font-[montserrat] lg:group-hover:translate-y-2 transition-all duration-500 mb-8">
+                {{ $service->sub_2 }}
+            </h4>
+
+            <!-- Button -->
+            <div class="hidden lg:flex absolute bottom-24 lg:group-hover:bottom-26 h-16 w-16 lg:group-hover:h-32 lg:group-hover:w-32 bg-black/0 border-2 border-gray-500 lg:group-hover:border-white/0 lg:group-hover:bg-black/50 items-center justify-center rounded-full transition-all duration-500">
+                <i class="icon-class lg:group-hover:hidden lg:group-hover:rotate-45 text-4xl bi bi-plus"></i>
+                <p class="hidden lg:group-hover:block text-white font-[Montserrat] text-xl">Go</p>
             </div>
         </div>
     </a>
     @endforeach
 </main>
+
 
 <script>
     const slides = document.querySelectorAll(".service-slide");

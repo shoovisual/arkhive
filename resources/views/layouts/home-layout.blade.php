36193414d995e737366a45c3022ea9e59e7a6595
shoovisual <shoovisual@gmail.com>
2025-06-23 21:58:00 +0300

    UI Updates v9.2.3

--- resources/views/layouts/home-layout.blade.php
+++ resources/views/layouts/home-layout.blade.php
@@ -86,6 +86,10 @@
     </script>
 </head>
 <body class="antialiased">
+    <!-- Preloader -->
+<div id="preloader" class="fixed inset-0 z-50 bg-ark-black flex items-center justify-center">
+  <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-ark-brown border-opacity-75"></div>
+</div>
     @include('sections.header')
     @yield('content')
     @if(!Request::is('/'))
@@ -95,7 +99,16 @@
 
 
     @include('layouts.vendor-js')
-
+<script>
+            window.addEventListener('load', function () {
+                const preloader = document.getElementById('preloader');
+                if (preloader) {
+                preloader.classList.add('opacity-0');
+                setTimeout(() => {
+                    preloader.style.display = 'none';
+                }, 500); // Wait for fade-out transition
+                }
+            });
 </body>
 </html>
 

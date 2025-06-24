781ec2362c81e53e8c0cd1569bcb9521e9a058e7
shoovisual <shoovisual@gmail.com>
2025-06-24 12:08:26 +0300

    Sitemap uploaded

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Illuminate\Console\Command;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a sitemap for the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Sitemap::create()
+        ->add(Url::create('/'))
+        ->add(Url::create('/about'))
+        ->add(Url::create('/contact'))
+        ->add(Url::create('/services'))
+        ->add(Url::create('/process'))
+        ->writeToFile(public_path('sitemap.xml'));
+
+    $this->info('Sitemap generated successfully!');
+    }
+}

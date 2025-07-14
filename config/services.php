22e516de7bcb7c25f7196a8c0a5736880e50e28c
shoovisual <shoovisual@gmail.com>
2025-07-14 23:09:22 +0300

    reCaptcha updated v.4.5

--- config/services.php
+++ config/services.php
@@ -28,11 +28,10 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
+    'recaptcha' => [
+    'site_key' => env('RECAPTCHA_SITE_KEY'),
+    'secret_key' => env('RECAPTCHA_SECRET_KEY'),
     ],
+
 
 ];
